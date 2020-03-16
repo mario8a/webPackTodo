e8abbd56f32d5e604f612d22a7ae3cc7d982f428
Mario Ochoa <mario8ato@hotmail.com>
2020-03-16 16:04:46 -0600

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './styles.css';
+
+
+import { saludar } from './js/componentes.js';
+import { Todo } from './classes/todo.class.js';
+
+
+const tarea = new Todo('Aprender javascript');
+
+console.log(tarea);
